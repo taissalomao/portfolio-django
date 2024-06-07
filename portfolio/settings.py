182baef1f72ef8d21ec526ab5e4b122a4b36e2d1
taissalomao <tais.salomao@fatec.sp.gov.br>
2024-06-06 21:20:14 -0300

    altera~ção na config de contato

--- portfolio/settings.py
+++ portfolio/settings.py
@@ -136,5 +136,5 @@ EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_USE_TLS = True
 EMAIL_PORT = 587
 EMAIL_HOST_USER = 'tgsoftwaresjc@gmail.com'
-EMAIL_HOST_PASSWORD = 'etxj fioi iqdw qvho'
+EMAIL_HOST_PASSWORD = 'ohtf rcwd ckmj rjkp'
 EMAIL_USE_SSL = False
